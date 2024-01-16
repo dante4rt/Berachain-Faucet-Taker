c604d0ebc861dd5360046747e14eab6c4946e430
dante4rt <rxmxdhxni@gmail.com>
2024-01-16 23:16:32 +0700

    refactor: update code to support old nodeJS ver

--- index.js
+++ index.js
@@ -3,74 +3,111 @@ const { CronJob } = require('cron');
 const readlineSync = require('readline-sync');
 require('colors');
 
-async function checkWebsite(address) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  const page = await browser.newPage();
-
-  await page.goto('https://artio.faucet.berachain.com/');
-
-  const termsCheckboxSelector = '#terms';
-  await page.waitForSelector(termsCheckboxSelector);
-  await page.click(termsCheckboxSelector);
-
-  const agreeButtonSelector = '//button[contains(text(), "I AGREE")]';
-  await page.waitForXPath(agreeButtonSelector);
-  const [agreeButton] = await page.$x(agreeButtonSelector);
-  if (agreeButton) {
-    await agreeButton.click();
-  }
-
-  const addressInputSelector =
-    'body > div:nth-child(12) > div.relative.flex.min-h-screen.w-full.flex-col.overflow-hidden.bg-background > main > div > div.flex.w-full.flex-col-reverse.items-center.justify-between.py-12.xl\\:flex-row > div > div.flex.flex-col.gap-1 > div.relative > div > input';
-  await page.waitForSelector(addressInputSelector);
-  await page.type(addressInputSelector, address);
-
-  const buttonSelector =
-    'body > div:nth-child(12) > div.relative.flex.min-h-screen.w-full.flex-col.overflow-hidden.bg-background > main > div > div.flex.w-full.flex-col-reverse.items-center.justify-between.py-12.xl\\:flex-row > div > button';
-  await page.waitForSelector(buttonSelector);
-  await page.click(buttonSelector);
-
-  await page.waitForTimeout(3000);
-
-  await page.click(buttonSelector);
-
-  await page.waitForSelector('div[role="alert"]', { timeout: 5000 });
-
-  const errorSelector = 'div[role="alert"].border-destructive-foreground';
-  const successSelector = 'div[role="alert"].border-success-foreground';
-
-  let errorElement = await page.$(errorSelector);
-  let successElement = await page.$(successSelector);
-
-  if (errorElement) {
-    let errorMessage = await page.evaluate(
-      (el) => (el ? el.textContent : 'Error element not found'),
-      errorElement
-    );
-    console.log('Error: '.red + errorMessage.red);
-    await browser.close();
-    return 'error';
-  } else if (successElement) {
-    let successMessage = await page.evaluate(
-      (el) => (el ? el.textContent : 'Success element not found'),
-      successElement
-    );
-    console.log('Success: '.green + successMessage.green);
-    await browser.close();
-    return 'success';
-  } else {
-    await browser.close();
-    return 'none';
-  }
+function checkWebsite(address, callback) {
+  puppeteer
+    .launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+    .then((browser) => {
+      browser.newPage().then((page) => {
+        page.goto('https://artio.faucet.berachain.com/').then(() => {
+          const termsCheckboxSelector = '#terms';
+          page.waitForSelector(termsCheckboxSelector).then(() => {
+            page.click(termsCheckboxSelector).then(() => {
+              const agreeButtonSelector =
+                '//button[contains(text(), "I AGREE")]';
+              page.waitForXPath(agreeButtonSelector).then(() => {
+                page.$x(agreeButtonSelector).then((agreeButtons) => {
+                  if (agreeButtons.length > 0) {
+                    agreeButtons[0].click().then(() => {
+                      const addressInputSelector =
+                        'body > div:nth-child(12) > div.relative.flex.min-h-screen.w-full.flex-col.overflow-hidden.bg-background > main > div > div.flex.w-full.flex-col-reverse.items-center.justify-between.py-12.xl\\:flex-row > div > div.flex.flex-col.gap-1 > div.relative > div > input';
+                      page.waitForSelector(addressInputSelector).then(() => {
+                        page.type(addressInputSelector, address).then(() => {
+                          const buttonSelector =
+                            'body > div:nth-child(12) > div.relative.flex.min-h-screen.w-full.flex-col.overflow-hidden.bg-background > main > div > div.flex.w-full.flex-col-reverse.items-center.justify-between.py-12.xl\\:flex-row > div > button';
+                          page.waitForSelector(buttonSelector).then(() => {
+                            page.click(buttonSelector).then(() => {
+                              page.waitForTimeout(3000).then(() => {
+                                page.click(buttonSelector).then(() => {
+                                  page
+                                    .waitForSelector('div[role="alert"]', {
+                                      timeout: 5000,
+                                    })
+                                    .then(() => {
+                                      page
+                                        .$(
+                                          'div[role="alert"].border-destructive-foreground'
+                                        )
+                                        .then((errorElement) => {
+                                          if (errorElement) {
+                                            page
+                                              .evaluate(
+                                                (el) => el.textContent,
+                                                errorElement
+                                              )
+                                              .then((errorMessage) => {
+                                                console.log(
+                                                  'Error: '.red +
+                                                    errorMessage.red
+                                                );
+                                                browser.close();
+                                                callback('error');
+                                              });
+                                          } else {
+                                            page
+                                              .$(
+                                                'div[role="alert"].border-success-foreground'
+                                              )
+                                              .then((successElement) => {
+                                                if (successElement) {
+                                                  page
+                                                    .evaluate(
+                                                      (el) => el.textContent,
+                                                      successElement
+                                                    )
+                                                    .then((successMessage) => {
+                                                      console.log(
+                                                        'Success: '.green +
+                                                          successMessage.green
+                                                      );
+                                                      browser.close();
+                                                      callback('success');
+                                                    });
+                                                } else {
+                                                  browser.close();
+                                                  callback('none');
+                                                }
+                                              });
+                                          }
+                                        })
+                                        .catch(() => {
+                                          browser.close();
+                                          callback('none');
+                                        });
+                                    });
+                                });
+                              });
+                            });
+                          });
+                        });
+                      });
+                    });
+                  }
+                });
+              });
+            });
+          });
+        });
+      });
+    });
 }
 
 function scheduleCronJob(address) {
   const job = new CronJob('0 */8 * * *', () => {
     console.log('Running cron job'.yellow);
-    checkWebsite(address).then((status) => {
+    checkWebsite(address, (status) => {
       console.log('Status:'.yellow, status.yellow);
     });
   });
@@ -81,7 +118,7 @@ function scheduleCronJob(address) {
 
 const address = readlineSync.question('Enter the wallet address: '.cyan);
 
-checkWebsite(address).then((status) => {
+checkWebsite(address, (status) => {
   console.log('Initial status:'.cyan, status.cyan);
   if (status !== 'none') {
     scheduleCronJob(address);
